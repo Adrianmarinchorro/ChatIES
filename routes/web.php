4c937c94543a2779c214e6e2c31f47071b2783f2
adrian <adrian.marin.chorro@gmail.com>
2023-04-11 12:26:55 +0200

    cambio en la ruta raiz

--- routes/web.php
+++ routes/web.php
@@ -14,54 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/test', function () {
-    $ldap_host = 'openldap';
-    $ldap_port = env('LDAP_PORT');
-    $ldap_dn = env('LDAP_USER_DN');
-    $ldap_password = env('LDAP_USER_PASSWORD');
-
-    $ldapconn = ldap_connect($ldap_host, $ldap_port);
-    ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-
-    if ($ldapconn) {
-        // Configurar opciones de la conexión, si es necesario
-        ldap_set_option($ldapconn, LDAP_OPT_PROTOCOL_VERSION, 3);
-        ldap_set_option($ldapconn, LDAP_OPT_REFERRALS, 0);
-
-        // Autenticarse con el servidor OpenLDAP, si es necesario
-        ldap_bind($ldapconn, "cn=admin,dc=example,dc=org", "admin");
-
-        // Realizar la búsqueda en el servidor OpenLDAP
-        $searchdn = "ou=usuario,dc=example,dc=org"; // DN del directorio donde se encuentran los usuarios
-        $filter = "(uid=amc)"; // Filtro de búsqueda para cn=admin y uid=pepe
-        $attributes = array("cn", "uid", "givenName", "sn"); // Atributos que quieres obtener del usuario
-
-        $search = ldap_search($ldapconn, $searchdn, $filter, $attributes);
-
-        if ($search) {
-            $entries = ldap_get_entries($ldapconn, $search);
-            // Obtener información del usuario de $entries
-            if ($entries['count'] > 0) {
-                $cn = $entries[0]['cn'][0];
-                $uid = $entries[0]['uid'][0];
-                echo "Nombre completo: " . $cn . "<br>";
-                echo "UID: " . $uid . "<br>";
-            } else {
-                echo "No se encontró información del usuario en el servidor OpenLDAP";
-            }
-        } else {
-            echo "No se pudo realizar la búsqueda del usuario en el servidor OpenLDAP";
-        }
-
-        // Cerrar la conexión al servidor OpenLDAP
-        ldap_close($ldapconn);
-    } else {
-        echo "No se pudo establecer la conexión con el servidor OpenLDAP";
-    }
-
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {

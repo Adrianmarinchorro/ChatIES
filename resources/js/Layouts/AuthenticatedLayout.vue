ba9c78017c2cbb8e7481632133e7db1798ed069f
adrian <adrian.marin.chorro@gmail.com>
2023-04-14 11:15:52 +0200

    creacion de componente vue de practica

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -32,6 +32,9 @@ const showingNavigationDropdown = ref(false);
                                 <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
                                     Dashboard
                                 </NavLink>
+                                <NavLink :href="route('prueba2')" :active="route().current('prueba2')">
+                                    Prueba
+                                </NavLink>
                             </div>
                         </div>
 
---
+++ resources/js/Pages/Prueba.vue
@@ -0,0 +1,13 @@
+<template>
+    <Head title="Prueba" />
+
+
+</template>
+
+
+
+<script setup> <!-- Setup es para que cargue primero el script y luego el template que es el html -->
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import {Head} from '@inertiajs/vue3';
+
+</script>

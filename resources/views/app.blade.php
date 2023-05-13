c208e7b671e739971db17a0ba2dfe88b392cf0fb
adrian <adrian.marin.chorro@gmail.com>
2023-05-13 19:56:18 +0200

    eliminar script

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <script src="https://kit.fontawesome.com/d0bc394077.js" crossorigin="anonymous"></script>
 
         <!-- Scripts -->
         @routes

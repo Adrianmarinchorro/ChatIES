ad45a0addc9618350a8f38aa49b3de6e27f9c0dd
adrian <adrian.marin.chorro@gmail.com>
2023-06-03 18:09:28 +0200

    test a medias de nuevo chat

--- tests/Feature/ChatIes/ChatTest.php
+++ tests/Feature/ChatIes/ChatTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\ChatIes;
 
 use App\Models\Chat;
+use App\Models\User;
 use Tests\TestCase;
 use App\Models\History;
 use Inertia\Testing\AssertableInertia as Assert;
@@ -28,20 +29,59 @@ class ChatTest extends TestCase
             'history_id' => $history->id
         ]);
 
-        // problema has
-        /*$this->get('/chat')
-            ->assertInertia(fn (Assert $page) => $page
-                ->component('Chat')
-                ->has('chats', fn (Assert $page) => $page
-                    ->where('id', $chat->id)
-                    ->where('data', $message)
-                )
-            );*/
-
         $this->get('/chat')
             ->assertInertia(fn (Assert $page) => $page
                 ->where('chats.id', $chat->id)
                 ->where('chats.data', json_decode($chat->data, true))
             );
+    }
+
+    public function test_user_can_chat_with_the_ai(): void
+    {
+        $this->signIn();
+
+        $response = $this->post('/chat', [
+           'message' => 'Hola que tal?'
+        ]);
+
+        $user = User::first();
+        $history = $user->history;
+        $chats = $history->chats;
+
+        $response->assertInertia(fn (Assert $page) => $page
+            ->where('chats.id', $chats[0]->id)
+            ->where('chats.data', json_decode($chats[0]->data, true)));
+    }
+
+    public function test_user_can_create_a_new_chat(): void
+    {
+        $this->signIn();
+
+        $response1 = $this->post('/chat', [
+            'message' => 'Hola que tal?'
+        ]);
+
+        $user = User::first();
+        $history = $user->history;
+        $chats = $history->chats;
+
+        $response1->assertInertia(fn (Assert $page) => $page
+            ->where('chats.id', $chats[0]->id)
+            ->where('chats.data', json_decode($chats[0]->data, true)));
+
+        $this->get('/newChat')->assertStatus(302)->assertRedirect('/chat');
+
+        $response2 = $this->post('/chat', ["message" => 'porque?'])->assertStatus(200);
+
+        $chats2 = $history->chats;
+
+        dd(History::first()->chats);
+
+        $response2->assertInertia(fn (Assert $page) => $page
+            ->whereNot('chats.id', $chats2[0]->id)
+            ->whereNot('chats.data', json_decode($chats2[0]->data, true))
+            >where('chats.id', $chats2[1]->id)
+        );
+
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,6 +3,7 @@
 namespace Tests;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Auth;
@@ -11,6 +12,7 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use RefreshDatabase;
+    use DatabaseMigrations;
 
     public function signIn(): void
     {

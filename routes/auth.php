6bfb98846dcfe0ab40b7b141589fbb69d82006bb
marmol89 <38738460+marmol89@users.noreply.github.com>
2023-06-08 20:24:17 +0200

    Update login

--- routes/auth.php
+++ routes/auth.php
@@ -10,7 +10,7 @@ Route::middleware('guest')->group(function () {
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
    /* Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');

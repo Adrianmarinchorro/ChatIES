ff60fba6d68e25f2d082a654320052c4029a9972
marmol89 <38738460+marmol89@users.noreply.github.com>
2023-06-12 20:14:56 +0200

    Arreglo login administrador

--- app/Ldap/Rules/OnlyAdministrators.php
+++ app/Ldap/Rules/OnlyAdministrators.php
@@ -3,6 +3,7 @@
 namespace App\Ldap\Rules;
 
 use Dotenv\Util\Str;
+use LdapRecord\Container;
 use LdapRecord\Laravel\Auth\Rule;
 
 class OnlyAdministrators extends Rule
@@ -12,10 +13,21 @@ class OnlyAdministrators extends Rule
      *
      * @return bool
      */
-    public function isValid()
+    public function isValid(): bool
     {
-        $cn = explode( ',', $this->user->getDn());
+        $ldap_group = env('LDAP_GROUP');
 
-        return true;
+        if (!isset($ldap_group)){
+            return false;
+        }
+
+        $gidNumber = explode( ',', $this->user->getFirstAttribute('gidNumber'));
+
+        $connection = Container::getDefaultConnection();
+        $query = $connection->query()->where('cn', $ldap_group);
+
+        $results = $query->get();
+
+        return $gidNumber[0] == $results[0]['gidnumber'][0];
     }
 }
